c24f9815697354f31adb0f02c769d239d147fc22
Chris Gottwaldt <cgottwaldt@x-es.com>
2023-11-21 14:48:39 -0600

    Add ability to start and stop cell generation.

--- gameoflife.py
+++ gameoflife.py
@@ -30,7 +30,8 @@ pygame.init()
 screen = pygame.display.set_mode((width, height))
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 clock = pygame.time.Clock()
-running = True
+running = True # Overall game is running
+generating = False # Cells are being drawn and removed
 
 # color creation
 background_color = Color(199, 229, 237)
@@ -41,6 +42,7 @@ colors = [dead_color, alive_color]
 # cell creation
 cell = pygame.Rect(width / 2, height / 2, cell_width, cell_height)
 corner = pygame.Rect(0, 0, cell_width, cell_height)
+
 
 # grid_lines = pygame.draw.rect(screen, alive_color, cell)
 
@@ -61,6 +63,64 @@ while running:
 
             cells[row // cell_width][col // cell_width] = not cells[row // cell_width][col // cell_width]
             cell = pygame.Rect(row, col, cell_width, cell_height)
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                # start and stop generating by using the space bar
+                generating = not generating
+
+    if generating:
+        # (src: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules)
+        # check all cells for the following conditions:
+        # Any live cell with fewer than two live neighbors dies by underpopulation.
+        # Any live cell with 2 or three live neighbors lives on to the next generation.
+        # Any live cell with > 3 live neighbours dies, as if by overpopulation.
+        # Any dead cell with 3 live neighbours becomes alive as if by reproduction.
+        num_adjacent = 0
+
+        for row in range(rows):
+            for col in range(cols):
+                # count its live neighbors :)
+                num_adjacent = 0
+                # check for live cells laterally (col to left and right!)
+                if col >= 1:
+                    if cells[row][col - 1]:
+                        num_adjacent += 1
+                if col < cols - 1:
+                    if cells[row][col + 1]:
+                        num_adjacent += 1
+                # and now vertically (row to left and right) and corners
+                if row >= 1:
+                    if cells[row - 1][col]:
+                        num_adjacent += 1
+                    if col >= 1:
+                        if cells[row - 1][col - 1]:
+                            num_adjacent += 1
+                    if col < cols - 1:
+                        if cells[row - 1][col + 1]:
+                            num_adjacent += 1
+                if row < rows - 1:
+                    if cells[row + 1][col]:
+                        num_adjacent += 1
+                    if col >= 1:
+                        if cells[row + 1][col - 1]:
+                            num_adjacent += 1
+                    if col < cols - 1:
+                        if cells[row + 1][col + 1]:
+                            num_adjacent += 1
+                # Any dead cell with 3 live neighbours -> alive as if by reproduction.
+                if not cells[row][col]:
+                    if num_adjacent == 3:
+                        cells[row][col] = True
+                else: # cell is alive
+                    # Any live cell with fewer than two live neighbors dies.
+                    if num_adjacent < 2:
+                        cells[row][col] = False
+                    # Any live cell with 2 or three live neighbors survives.
+                    elif num_adjacent == 2 or num_adjacent == 3:
+                        cells[row][col] = True
+                    # Any live cell with > 3 live neighbours dies.
+                    elif num_adjacent > 3:
+                        cells[row][col] = False
 
 
 
@@ -76,6 +136,7 @@ while running:
                 pygame.draw.rect(screen, colors[0], cell)
 
 
+
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
@@ -85,55 +146,55 @@ while running:
 
 pygame.quit()
 
-# (src: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules)
-# check all cells for the following conditions:
-# Any live cell with fewer than two live neighbors dies by underpopulation.
-# Any live cell with 2 or three live neighbors lives on to the next generation.
-# Any live cell with > 3 live neighbours dies, as if by overpopulation.
-# Any dead cell with 3 live neighbours becomes alive as if by reproduction.
-num_adjacent = 0
+# # (src: https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life#Rules)
+# # check all cells for the following conditions:
+# # Any live cell with fewer than two live neighbors dies by underpopulation.
+# # Any live cell with 2 or three live neighbors lives on to the next generation.
+# # Any live cell with > 3 live neighbours dies, as if by overpopulation.
+# # Any dead cell with 3 live neighbours becomes alive as if by reproduction.
+# num_adjacent = 0
 
-for row in range(rows):
-    for col in range(cols):
-        # count its live neighbors :)
-        num_adjacent = 0
-        # check for live cells laterally (col to left and right!)
-        if col >= 1:
-            if cells[row][col - 1]:
-                num_adjacent += 1
-        if col < cols - 1:
-            if cells[row][col + 1]:
-                num_adjacent += 1
-        # and now vertically (row to left and right) and corners
-        if row >= 1:
-            if cells[row - 1][col]:
-                num_adjacent += 1
-            if col >= 1:
-                if cells[row - 1][col - 1]:
-                    num_adjacent += 1
-            if col < cols - 1:
-                if cells[row - 1][col + 1]:
-                    num_adjacent += 1
-        if row < rows - 1:
-            if cells[row + 1][col]:
-                num_adjacent += 1
-            if col >= 1:
-                if cells[row + 1][col - 1]:
-                    num_adjacent += 1
-            if col < cols - 1:
-                if cells[row + 1][col + 1]:
-                    num_adjacent += 1
-        # Any dead cell with 3 live neighbours -> alive as if by reproduction.
-        if not cells[row][col]:
-            if num_adjacent == 3:
-                cells[row][col] = True
-        else: # cell is alive
-            # Any live cell with fewer than two live neighbors dies.
-            if num_adjacent < 2:
-                cells[row][col] = False
-            # Any live cell with 2 or three live neighbors survives.
-            elif num_adjacent == 2 or num_adjacent == 3:
-                cells[row][col] = True
-            # Any live cell with > 3 live neighbours dies.
-            elif num_adjacent > 3:
-                cells[row][col] = False
+# for row in range(rows):
+#     for col in range(cols):
+#         # count its live neighbors :)
+#         num_adjacent = 0
+#         # check for live cells laterally (col to left and right!)
+#         if col >= 1:
+#             if cells[row][col - 1]:
+#                 num_adjacent += 1
+#         if col < cols - 1:
+#             if cells[row][col + 1]:
+#                 num_adjacent += 1
+#         # and now vertically (row to left and right) and corners
+#         if row >= 1:
+#             if cells[row - 1][col]:
+#                 num_adjacent += 1
+#             if col >= 1:
+#                 if cells[row - 1][col - 1]:
+#                     num_adjacent += 1
+#             if col < cols - 1:
+#                 if cells[row - 1][col + 1]:
+#                     num_adjacent += 1
+#         if row < rows - 1:
+#             if cells[row + 1][col]:
+#                 num_adjacent += 1
+#             if col >= 1:
+#                 if cells[row + 1][col - 1]:
+#                     num_adjacent += 1
+#             if col < cols - 1:
+#                 if cells[row + 1][col + 1]:
+#                     num_adjacent += 1
+#         # Any dead cell with 3 live neighbours -> alive as if by reproduction.
+#         if not cells[row][col]:
+#             if num_adjacent == 3:
+#                 cells[row][col] = True
+#         else: # cell is alive
+#             # Any live cell with fewer than two live neighbors dies.
+#             if num_adjacent < 2:
+#                 cells[row][col] = False
+#             # Any live cell with 2 or three live neighbors survives.
+#             elif num_adjacent == 2 or num_adjacent == 3:
+#                 cells[row][col] = True
+#             # Any live cell with > 3 live neighbours dies.
+#             elif num_adjacent > 3:
+#                 cells[row][col] = False
